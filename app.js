fe9c710cd5cf52e220b22c0ab5475ae0ed553490
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2026-03-25 15:49:00 +0100

    Add .gitignore and test PouchDB sync with CouchDB

---
+++ app.js
@@ -0,0 +1,26 @@
+const localDB = new PouchDB('offline-first-demo')
+const remoteDB = 'http://localhost:5984/offline-first-demo'
+
+localDB.put({
+    _id: 'test',
+    text: 'Hello offline world'
+}).then(function(response) {
+    console.log('Locally saved!', response)
+}).catch(function(e){
+    console.log('Error: ', e)
+})
+
+localDB.put({
+    _id: 'test2',
+    text: 'Hello again offline world'
+}).then(function(response) {
+    console.log('Locally saved!', response)
+}).catch(function(e){
+    console.log('Error: ', e)
+})
+
+localDB.sync(remoteDB, {live: true}).on('change', function(change) {
+    console.log('Synced!', change)
+}).on('error', function(e){
+    console.log('Sync error: ', e)
+})
