0daaa9d6190b0a0911ddade5b3e57f816a886dab
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2026-03-31 08:45:29 +0200

    Add color picker and reuse same header dokument from db

--- app.js
+++ app.js
@@ -1,39 +1,53 @@
-const localDB = new PouchDB('offline-first-demo')
-const remoteDB = 'http://localhost:5984/offline-first-demo'
+const localDB = new PouchDB("offline-first-demo");
+const remoteDB = "http://localhost:5984/offline-first-demo";
 
-const headerText = document.getElementById('header');
-const textInput = document.getElementById('textInput');
-const button = document.getElementById('saveChangeButton');
+const headerText = document.getElementById("header");
+const textInput = document.getElementById("textInput");
+const colorInput = document.getElementById("colorInput");
+const button = document.getElementById("saveChangeButton");
 
-localDB.sync(remoteDB, {live: true, retry: true
-}).on('change', function(change) {
-    console.log('Synced!', change)
-}).on('error', function(e){
-    console.log('Sync error: ', e)
-})
+localDB
+  .sync(remoteDB, { live: true, retry: true })
+  .on("change", function (change) {
+    console.log("Synced!", change);
+  })
+  .on("error", function (e) {
+    console.log("Sync error: ", e);
+  });
+
+localDB
+  .get("header")
+  .then(function (doc) {
+    headerText.textContent = doc.text;
+    headerText.style.color = doc.color;
+  })
+  .catch(function (e) {
+    if (e.name == "not_found") {
+      return localDB.put({
+        _id: "header",
+        text: headerText.textContent,
+        color: headerText.style.color || "#000000",
+      });
+    }
+  });
 
 button.onclick = () => {
-    headerText.textContent = textInput.value
-
-    localDB.post({
-        text: textInput.value,
-        timestamp: new Date().getTime()
-    })
-    .then(function(response) {
-        console.log('Locally saved!', response)
-    })
-    .catch(function(e) {
-        console.log('Error: ', e)
-    })
+  localDB.get("header").then(function (doc) {
+    return localDB
+      .put({
+        _id: "header",
+        _rev: doc._rev,
+        text: textInput.value || doc.text,
+        color: colorInput.value || doc.color,
+        timestamp: new Date().getTime(),
+      })
+      .then(function (response) {
+        headerText.textContent = textInput.value || doc.text;
+        headerText.style.color = colorInput.value || doc.color;
+        console.log("Locally saved!", doc);
+      })
+      .catch(function (e) {
+        console.log("Error", e);
+      });
+  });
 };
-
-/*
-localDB.put({
-    _id: 'test',
-    text: 'Hello offline world'
-}).then(function(response) {
-    console.log('Locally saved!', response)
-}).catch(function(e){
-    console.log('Error: ', e)
-})
-*/
